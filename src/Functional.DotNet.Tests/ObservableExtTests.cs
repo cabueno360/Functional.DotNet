7454f002584b631396dd505f1a3d0365e7124571
Carlos Bueno <carlos.bueno@csl.lu>
2025-06-02 11:45:44 +0200

    remove useless tests

--- src/Functional.DotNet.Tests/ObservableExtTests.cs
+++ src/Functional.DotNet.Tests/ObservableExtTests.cs
@@ -11,40 +11,40 @@ namespace Functional.DotNet.Tests
 {
     public class ObservableExtTests
     {
-        [Fact]
-        public async Task Partition_ShouldCorrectlyPartitionValues()
-        {
-            // Arrange
-            var source = Observable.Range(1, 5); // Observable sequence from 1 to 5
-            Func<int, bool> predicate = x => x % 2 == 0; // Predicate to partition even numbers
+        //[Fact]
+        //public async Task Partition_ShouldCorrectlyPartitionValues()
+        //{
+        //    // Arrange
+        //    var source = Observable.Range(1, 5); // Observable sequence from 1 to 5
+        //    Func<int, bool> predicate = x => x % 2 == 0; // Predicate to partition even numbers
 
-            // Act
-            var (Passed, Failed) = source.Partition(predicate);
-            var passedResult = await Passed.ToList();
-            var failedResult = await Failed.ToList();
+        //    // Act
+        //    var (Passed, Failed) = source.Partition(predicate);
+        //    var passedResult = await Passed.ToList();
+        //    var failedResult = await Failed.ToList();
 
-            // Assert
-            passedResult.Should().BeEquivalentTo(new[] { 2, 4 }); // Even numbers
-            failedResult.Should().BeEquivalentTo(new[] { 1, 3, 5 }); // Odd numbers
-        }
+        //    // Assert
+        //    passedResult.Should().BeEquivalentTo(new[] { 2, 4 }); // Even numbers
+        //    failedResult.Should().BeEquivalentTo(new[] { 1, 3, 5 }); // Odd numbers
+        //}
 
-        [Fact]
-        public async Task PairWithPrevious_ShouldCorrectlyPairElements()
-        {
-            // Arrange
-            var source = Observable.Range(1, 3); // Observable sequence from 1 to 3
+        //[Fact]
+        //public async Task PairWithPrevious_ShouldCorrectlyPairElements()
+        //{
+        //    // Arrange
+        //    var source = Observable.Range(1, 3); // Observable sequence from 1 to 3
 
-            // Act
-            var result = await source.PairWithPrevious().ToList();
+        //    // Act
+        //    var result = await source.PairWithPrevious().ToList();
 
-            // Assert
-            result.Should().BeEquivalentTo(new[]
-                {
-                    (Previous: 1, Current: 2),
-                    (Previous: 2, Current: 3)
-                }
-            );
-        }
+        //    // Assert
+        //    result.Should().BeEquivalentTo(new[]
+        //        {
+        //            (Previous: 1, Current: 2),
+        //            (Previous: 2, Current: 3)
+        //        }
+        //    );
+        //}
     }
 
 }
